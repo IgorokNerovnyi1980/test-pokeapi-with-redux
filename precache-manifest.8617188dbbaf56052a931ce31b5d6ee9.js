13ceed7bcb7649c75da908bb186aeeaefea95c5f
IgorokNerovnyi1980 <i.nerovnyi@gmail.com>
2019-08-07 16:40:34 +0300

    Updates

---
+++ precache-manifest.8617188dbbaf56052a931ce31b5d6ee9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26947768fc5113f7e1056beb8900354d",
+    "url": "/test-pokeapi/index.html"
+  },
+  {
+    "revision": "b7dc5d2bb4c2f9bd1c84",
+    "url": "/test-pokeapi/static/css/main.08da0c2b.chunk.css"
+  },
+  {
+    "revision": "cf43ec390464489ce353",
+    "url": "/test-pokeapi/static/js/2.0469a4ee.chunk.js"
+  },
+  {
+    "revision": "b7dc5d2bb4c2f9bd1c84",
+    "url": "/test-pokeapi/static/js/main.8a5e9d83.chunk.js"
+  },
+  {
+    "revision": "17112a46ca852e249431",
+    "url": "/test-pokeapi/static/js/runtime~main.afde0772.js"
+  }
+]);

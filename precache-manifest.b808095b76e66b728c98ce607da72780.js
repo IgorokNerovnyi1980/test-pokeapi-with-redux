877f6a935f92a8ad8039514612a873d8548aba90
IgorokNerovnyi1980 <i.nerovnyi@gmail.com>
2019-08-08 08:54:22 +0300

    Updates

---
+++ precache-manifest.b808095b76e66b728c98ce607da72780.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec81e391989aa9da3cf7584d64ba25bb",
+    "url": "/test-pokeapi-with-redux/index.html"
+  },
+  {
+    "revision": "57c15eadbc7278f6e02c",
+    "url": "/test-pokeapi-with-redux/static/css/main.f9b9e1c5.chunk.css"
+  },
+  {
+    "revision": "0915603a036154e67cf1",
+    "url": "/test-pokeapi-with-redux/static/js/2.f964895e.chunk.js"
+  },
+  {
+    "revision": "57c15eadbc7278f6e02c",
+    "url": "/test-pokeapi-with-redux/static/js/main.f3b25053.chunk.js"
+  },
+  {
+    "revision": "59b055659c33614f7a9c",
+    "url": "/test-pokeapi-with-redux/static/js/runtime~main.b13b41b4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/test-pokeapi/precache-manifest.40535c550bdaf6e223a1e092994a5873.js"
+  "/test-pokeapi-with-redux/precache-manifest.b808095b76e66b728c98ce607da72780.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test-pokeapi/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test-pokeapi-with-redux/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
